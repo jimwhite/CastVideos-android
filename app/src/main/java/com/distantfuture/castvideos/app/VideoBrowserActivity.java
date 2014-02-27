b47fe020a94bce54eae0c19c9ac27707490e8ff7
Steve Gehrman <sgehrman@gmail.com>
2014-02-26 23:55:51 -0800

    updated to latest app code

--- app/src/main/java/com/distantfuture/castvideos/app/VideoBrowserActivity.java
+++ app/src/main/java/com/distantfuture/castvideos/app/VideoBrowserActivity.java
@@ -16,6 +16,12 @@
 
 package com.distantfuture.castvideos.app;
 
+import com.distantfuture.castcompanionlibrary.lib.cast.VideoCastManager;
+import com.distantfuture.castcompanionlibrary.lib.cast.callbacks.IVideoCastConsumer;
+import com.distantfuture.castcompanionlibrary.lib.cast.callbacks.VideoCastConsumerImpl;
+import com.distantfuture.castcompanionlibrary.lib.widgets.MiniController;
+import com.distantfuture.castvideos.app.settings.CastPreference;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -27,163 +33,157 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.distantfuture.castcompanionlibrary.lib.cast.VideoCastManager;
-import com.distantfuture.castcompanionlibrary.lib.cast.callbacks.IVideoCastConsumer;
-import com.distantfuture.castcompanionlibrary.lib.cast.callbacks.VideoCastConsumerImpl;
-import com.distantfuture.castcompanionlibrary.lib.widgets.MiniController;
-import com.distantfuture.castvideos.app.settings.CastPreference;
-
 public class VideoBrowserActivity extends ActionBarActivity {
 
-  private static final String TAG = "VideoBrowserActivity";
-  private VideoCastManager mCastManager;
-  private IVideoCastConsumer mCastConsumer;
-  private MiniController mMini;
-  private MenuItem mediaRouteMenuItem;
+    private static final String TAG = "VideoBrowserActivity";
+    private VideoCastManager mCastManager;
+    private IVideoCastConsumer mCastConsumer;
+    private MiniController mMini;
+    private MenuItem mediaRouteMenuItem;
 
-  /*
-   * (non-Javadoc)
-   * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
-   */
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    VideoCastManager.checkGooglePlaySevices(this);
-    setContentView(R.layout.video_browser);
-    ActionBar actionBar = getSupportActionBar();
+    /*
+     * (non-Javadoc)
+     * @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        VideoCastManager.checkGooglePlaySevices(this);
+        setContentView(R.layout.video_browser);
+        ActionBar actionBar = getSupportActionBar();
 
-    mCastManager = CastApplication.getCastManager(this);
+        mCastManager = CastApplication.getCastManager(this);
 
-    // -- Adding MiniController
-    mMini = (MiniController) findViewById(R.id.miniController1);
-    mCastManager.addMiniController(mMini);
+        // -- Adding MiniController
+        mMini = (MiniController) findViewById(R.id.miniController1);
+        mCastManager.addMiniController(mMini);
 
-    mCastConsumer = new VideoCastConsumerImpl() {
-
-      @Override
-      public void onFailed(int resourceId, int statusCode) {
-
-      }
-
-      @Override
-      public void onConnectionSuspended(int cause) {
-        Log.d(TAG, "onConnectionSuspended() was called with cause: " + cause);
-        com.distantfuture.castvideos.app.utils.Utils.
-            showToast(VideoBrowserActivity.this, R.string.connection_temp_lost);
-      }
-
-      @Override
-      public void onConnectivityRecovered() {
-        com.distantfuture.castvideos.app.utils.Utils.
-            showToast(VideoBrowserActivity.this, R.string.connection_recovered);
-      }
-
-      @Override
-      public void onCastDeviceDetected(final RouteInfo info) {
-        if (!CastPreference.isFtuShown(VideoBrowserActivity.this)) {
-          CastPreference.setFtuShown(VideoBrowserActivity.this);
-
-          Log.d(TAG, "Route is visible: " + info);
-          new Handler().postDelayed(new Runnable() {
+        mCastConsumer = new VideoCastConsumerImpl() {
 
             @Override
-            public void run() {
-              if (mediaRouteMenuItem.isVisible()) {
-                Log.d(TAG, "Cast Icon is visible: " + info.getName());
-                showFtu();
-              }
+            public void onFailed(int resourceId, int statusCode) {
+
             }
-          }, 1000);
+
+            @Override
+            public void onConnectionSuspended(int cause) {
+                Log.d(TAG, "onConnectionSuspended() was called with cause: " + cause);
+                com.distantfuture.castvideos.app.utils.Utils.
+                        showToast(VideoBrowserActivity.this, R.string.connection_temp_lost);
+            }
+
+            @Override
+            public void onConnectivityRecovered() {
+                com.distantfuture.castvideos.app.utils.Utils.
+                        showToast(VideoBrowserActivity.this, R.string.connection_recovered);
+            }
+
+            @Override
+            public void onCastDeviceDetected(final RouteInfo info) {
+                if (!CastPreference.isFtuShown(VideoBrowserActivity.this)) {
+                    CastPreference.setFtuShown(VideoBrowserActivity.this);
+
+                    Log.d(TAG, "Route is visible: " + info);
+                    new Handler().postDelayed(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            if (mediaRouteMenuItem.isVisible()) {
+                                Log.d(TAG, "Cast Icon is visible: " + info.getName());
+                                showFtu();
+                            }
+                        }
+                    }, 1000);
+                }
+            }
+        };
+
+        setupActionBar(actionBar);
+        mCastManager.reconnectSessionIfPossible(this, false);
+    }
+
+    private void setupActionBar(ActionBar actionBar) {
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        getSupportActionBar().setIcon(R.drawable.actionbar_logo_castvideos);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        mediaRouteMenuItem = mCastManager.
+                addMediaRouterButton(menu, R.id.media_route_menu_item);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent i = new Intent(VideoBrowserActivity.this, CastPreference.class);
+                startActivity(i);
+                break;
         }
-      }
-    };
-
-    setupActionBar(actionBar);
-    mCastManager.reconnectSessionIfPossible(this, false);
-  }
-
-  private void setupActionBar(ActionBar actionBar) {
-    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-    getSupportActionBar().setIcon(R.drawable.actionbar_logo_castvideos);
-    getSupportActionBar().setDisplayShowTitleEnabled(false);
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    super.onCreateOptionsMenu(menu);
-    getMenuInflater().inflate(R.menu.main, menu);
-
-    mediaRouteMenuItem = mCastManager.
-        addMediaRouterButton(menu, R.id.media_route_menu_item);
-
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    switch (item.getItemId()) {
-      case R.id.action_settings:
-        Intent i = new Intent(VideoBrowserActivity.this, CastPreference.class);
-        startActivity(i);
-        break;
-    }
-    return true;
-  }
-
-  private void showFtu() {
-  }
-
-  @Override
-  public boolean onKeyDown(int keyCode, KeyEvent event) {
-    if (!mCastManager.isConnected()) {
-      return super.onKeyDown(keyCode, event);
-    } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
-      changeVolume(CastApplication.VOLUME_INCREMENT);
-    } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
-      changeVolume(-CastApplication.VOLUME_INCREMENT);
-    } else {
-      return super.onKeyDown(keyCode, event);
-    }
-    return true;
-  }
-
-  private void changeVolume(double volumeIncrement) {
-    if (mCastManager == null) {
-      return;
-    }
-    try {
-      mCastManager.incrementVolume(volumeIncrement);
-    } catch (Exception e) {
-      Log.e(TAG, "onVolumeChange() Failed to change volume", e);
-      com.distantfuture.castvideos.app.utils.Utils.handleException(this, e);
-    }
-  }
-
-  @Override
-  protected void onResume() {
-    Log.d(TAG, "onResume() was called");
-    mCastManager = CastApplication.getCastManager(this);
-    if (null != mCastManager) {
-      mCastManager.addVideoCastConsumer(mCastConsumer);
-      mCastManager.incrementUiCounter();
+        return true;
     }
 
-    super.onResume();
-  }
-
-  @Override
-  protected void onPause() {
-    mCastManager.decrementUiCounter();
-    mCastManager.removeVideoCastConsumer(mCastConsumer);
-    super.onPause();
-  }
-
-  @Override
-  protected void onDestroy() {
-    if (null != mCastManager) {
-      mMini.removeOnMiniControllerChangedListener(mCastManager);
+    private void showFtu() {
     }
-    super.onDestroy();
-  }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (!mCastManager.isConnected()) {
+            return super.onKeyDown(keyCode, event);
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            changeVolume(CastApplication.VOLUME_INCREMENT);
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            changeVolume(-CastApplication.VOLUME_INCREMENT);
+        } else {
+            return super.onKeyDown(keyCode, event);
+        }
+        return true;
+    }
+
+    private void changeVolume(double volumeIncrement) {
+        if (mCastManager == null) {
+            return;
+        }
+        try {
+            mCastManager.incrementVolume(volumeIncrement);
+        } catch (Exception e) {
+            Log.e(TAG, "onVolumeChange() Failed to change volume", e);
+            com.distantfuture.castvideos.app.utils.Utils.handleException(this, e);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume() was called");
+        mCastManager = CastApplication.getCastManager(this);
+        if (null != mCastManager) {
+            mCastManager.addVideoCastConsumer(mCastConsumer);
+            mCastManager.incrementUiCounter();
+        }
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mCastManager.decrementUiCounter();
+        mCastManager.removeVideoCastConsumer(mCastConsumer);
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (null != mCastManager) {
+            mMini.removeOnMiniControllerChangedListener(mCastManager);
+        }
+        super.onDestroy();
+    }
 
 }
--- app/src/main/java/com/distantfuture/castvideos/app/browser/VideoListAdapter.java
+++ app/src/main/java/com/distantfuture/castvideos/app/browser/VideoListAdapter.java
@@ -16,6 +16,11 @@
 
 package com.distantfuture.castvideos.app.browser;
 
+import com.androidquery.AQuery;
+import com.google.android.gms.cast.MediaInfo;
+import com.google.android.gms.cast.MediaMetadata;
+import com.distantfuture.castvideos.app.R;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,11 +29,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.androidquery.AQuery;
-import com.distantfuture.castvideos.app.R;
-import com.google.android.gms.cast.MediaInfo;
-import com.google.android.gms.cast.MediaMetadata;
-
 import java.util.List;
 
 /**
@@ -36,66 +36,63 @@ import java.util.List;
  */
 public class VideoListAdapter extends ArrayAdapter<MediaInfo> {
 
-  private final Context mContext;
-  private final float mAspectRatio = 9f / 16f;
+    private final Context mContext;
+    private final float mAspectRatio = 9f / 16f;
 
-  /**
-   * @param context
-   * @param resource
-   */
-  public VideoListAdapter(Context context) {
-    super(context, 0);
-    this.mContext = context;
-  }
-
-  /*
-   * (non-Javadoc)
-   * @see android.widget.ArrayAdapter#getView(int, android.view.View, android.view.ViewGroup)
-   */
-  @Override
-  public View getView(int position, View convertView, ViewGroup parent) {
-
-    ViewHolder holder;
-    LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    MediaMetadata mm = getItem(position).getMetadata();
-
-    if (convertView == null) {
-      convertView = inflater.inflate(R.layout.browse_row, null);
-      holder = new ViewHolder();
-      holder.imgView = (ImageView) convertView.findViewById(R.id.imageView1);
-      holder.titleView = (TextView) convertView.findViewById(R.id.textView1);
-      holder.descrView = (TextView) convertView.findViewById(R.id.textView2);
-      convertView.setTag(holder);
-    } else {
-      holder = (ViewHolder) convertView.getTag();
+    /**
+     * @param context
+     * @param resource
+     */
+    public VideoListAdapter(Context context) {
+        super(context, 0);
+        this.mContext = context;
     }
 
-    AQuery aq = new AQuery(convertView);
-    aq.id(holder.imgView)
-        .width(110)
-        .image(mm.getImages()
-            .get(0)
-            .getUrl()
-            .toString(), true, true, 0, R.drawable.default_video, null, 0, mAspectRatio);
-    aq.id(holder.titleView).text(mm.getString(MediaMetadata.KEY_TITLE));
-    aq.id(holder.descrView).text(mm.getString(MediaMetadata.KEY_SUBTITLE));
+    /*
+     * (non-Javadoc)
+     * @see android.widget.ArrayAdapter#getView(int, android.view.View, android.view.ViewGroup)
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-    return convertView;
-  }
+        ViewHolder holder;
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        MediaMetadata mm = getItem(position).getMetadata();
 
-  private class ViewHolder {
-    TextView titleView;
-    TextView descrView;
-    ImageView imgView;
-  }
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.browse_row, null);
+            holder = new ViewHolder();
+            holder.imgView = (ImageView) convertView.findViewById(R.id.imageView1);
+            holder.titleView = (TextView) convertView.findViewById(R.id.textView1);
+            holder.descrView = (TextView) convertView.findViewById(R.id.textView2);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
 
-  public void setData(List<MediaInfo> data) {
-    clear();
-    if (data != null) {
-      for (MediaInfo item : data) {
-        add(item);
-      }
+        AQuery aq = new AQuery(convertView);
+        aq.id(holder.imgView).width(110).image(mm.getImages().get(0).getUrl().toString(),
+                true, true, 0, R.drawable.default_video, null, 0, mAspectRatio);
+        aq.id(holder.titleView).text(mm.getString(MediaMetadata.KEY_TITLE));
+        aq.id(holder.descrView).text(mm.getString(MediaMetadata.KEY_SUBTITLE));
+
+        return convertView;
     }
 
-  }
+    private class ViewHolder {
+        TextView titleView;
+        TextView descrView;
+        ImageView imgView;
+    }
+
+    public void setData(List<MediaInfo> data) {
+        clear();
+        if (data != null) {
+            for (MediaInfo item : data) {
+                add(item);
+            }
+        }
+
+    }
 }
