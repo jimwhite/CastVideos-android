84c48771bbc94b2aee8942246e3d551334f53ad9
Steve Gehrman <sgehrman@gmail.com>
2014-02-27 00:08:23 -0800

    reformatted

--- app/src/main/java/com/distantfuture/castvideos/app/browser/VideoBrowserListFragment.java
+++ app/src/main/java/com/distantfuture/castvideos/app/browser/VideoBrowserListFragment.java
@@ -16,11 +16,6 @@
 
 package com.distantfuture.castvideos.app.browser;
 
-import com.distantfuture.castcompanionlibrary.lib.utils.Utils;
-import com.distantfuture.castvideos.app.R;
-import com.google.android.gms.cast.MediaInfo;
-import com.distantfuture.castvideos.app.mediaplayer.LocalPlayerActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
@@ -29,90 +24,92 @@ import android.support.v4.content.Loader;
 import android.view.View;
 import android.widget.ListView;
 
+import com.distantfuture.castcompanionlibrary.lib.utils.Utils;
+import com.distantfuture.castvideos.app.R;
+import com.distantfuture.castvideos.app.mediaplayer.LocalPlayerActivity;
+import com.google.android.gms.cast.MediaInfo;
+
 import java.util.List;
 
-public class VideoBrowserListFragment extends ListFragment implements
-        LoaderManager.LoaderCallbacks<List<MediaInfo>> {
+public class VideoBrowserListFragment extends ListFragment implements LoaderManager.LoaderCallbacks<List<MediaInfo>> {
 
-    private static final String CATALOG_URL =
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/" +
-                    "videos-enhanced-b.json";
-    private VideoListAdapter mAdapter;
+  private static final String CATALOG_URL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/" + "videos-enhanced-b.json";
+  private VideoListAdapter mAdapter;
 
-    /*
-     * (non-Javadoc)
-     * @see android.support.v4.app.Fragment#onActivityCreated(android.os.Bundle)
-     */
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        getListView().setFastScrollEnabled(true);
-        mAdapter = new VideoListAdapter(getActivity());
-        setEmptyText(getString(R.string.no_video_found));
-        setListAdapter(mAdapter);
-        setListShown(false);
-        getLoaderManager().initLoader(0, null, this);
+  /*
+   * (non-Javadoc)
+   * @see android.support.v4.app.Fragment#onActivityCreated(android.os.Bundle)
+   */
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    getListView().setFastScrollEnabled(true);
+    mAdapter = new VideoListAdapter(getActivity());
+    setEmptyText(getString(R.string.no_video_found));
+    setListAdapter(mAdapter);
+    setListShown(false);
+    getLoaderManager().initLoader(0, null, this);
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see android.support.v4.app.LoaderManager.LoaderCallbacks#onLoadFinished(android
+   * .support.v4.content.Loader, java.lang.Object)
+   */
+  @Override
+  public void onLoadFinished(Loader<List<MediaInfo>> arg0, List<MediaInfo> data) {
+    mAdapter.setData(data);
+    if (isResumed()) {
+      setListShown(true);
+    } else {
+      setListShownNoAnimation(true);
     }
+  }
 
-    /*
-     * (non-Javadoc)
-     * @see android.support.v4.app.LoaderManager.LoaderCallbacks#onLoadFinished(android
-     * .support.v4.content.Loader, java.lang.Object)
-     */
-    @Override
-    public void onLoadFinished(Loader<List<MediaInfo>> arg0, List<MediaInfo> data) {
-        mAdapter.setData(data);
-        if (isResumed()) {
-            setListShown(true);
-        } else {
-            setListShownNoAnimation(true);
-        }
-    }
+  /*
+   * (non-Javadoc)
+   * @see android.support.v4.app.LoaderManager.LoaderCallbacks#onLoaderReset(android
+   * .support.v4.content.Loader)
+   */
+  @Override
+  public void onLoaderReset(Loader<List<MediaInfo>> arg0) {
+    mAdapter.setData(null);
+  }
 
-    /*
-     * (non-Javadoc)
-     * @see android.support.v4.app.LoaderManager.LoaderCallbacks#onLoaderReset(android
-     * .support.v4.content.Loader)
-     */
-    @Override
-    public void onLoaderReset(Loader<List<MediaInfo>> arg0) {
-        mAdapter.setData(null);
-    }
+  @Override
+  public void onListItemClick(ListView l, View v, int position, long id) {
+    MediaInfo selectedMedia = mAdapter.getItem(position);
+    handleNavigation(selectedMedia, false);
+  }
 
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        MediaInfo selectedMedia = mAdapter.getItem(position);
-        handleNavigation(selectedMedia, false);
-    }
+  private void handleNavigation(MediaInfo info, boolean autoStart) {
+    Intent intent = new Intent(getActivity(), LocalPlayerActivity.class);
+    intent.putExtra("media", Utils.fromMediaInfo(info));
+    intent.putExtra("shouldStart", autoStart);
+    getActivity().startActivity(intent);
+  }
 
-    private void handleNavigation(MediaInfo info, boolean autoStart) {
-        Intent intent = new Intent(getActivity(), LocalPlayerActivity.class);
-        intent.putExtra("media", Utils.fromMediaInfo(info));
-        intent.putExtra("shouldStart", autoStart);
-        getActivity().startActivity(intent);
-    }
+  /*
+   * (non-Javadoc)
+   * @see android.support.v4.app.LoaderManager.LoaderCallbacks#onCreateLoader(int,
+   * android.os.Bundle)
+   */
+  @Override
+  public Loader<List<MediaInfo>> onCreateLoader(int arg0, Bundle arg1) {
+    return new VideoItemLoader(getActivity(), CATALOG_URL);
+  }
 
-    /*
-     * (non-Javadoc)
-     * @see android.support.v4.app.LoaderManager.LoaderCallbacks#onCreateLoader(int,
-     * android.os.Bundle)
-     */
-    @Override
-    public Loader<List<MediaInfo>> onCreateLoader(int arg0, Bundle arg1) {
-        return new VideoItemLoader(getActivity(), CATALOG_URL);
-    }
+  public static VideoBrowserListFragment newInstance() {
+    VideoBrowserListFragment f = new VideoBrowserListFragment();
+    Bundle b = new Bundle();
+    f.setArguments(b);
+    return f;
+  }
 
-    public static VideoBrowserListFragment newInstance() {
-        VideoBrowserListFragment f = new VideoBrowserListFragment();
-        Bundle b = new Bundle();
-        f.setArguments(b);
-        return f;
-    }
-
-    public static VideoBrowserListFragment newInstance(Bundle b) {
-        VideoBrowserListFragment f = new VideoBrowserListFragment();
-        f.setArguments(b);
-        return f;
-    }
+  public static VideoBrowserListFragment newInstance(Bundle b) {
+    VideoBrowserListFragment f = new VideoBrowserListFragment();
+    f.setArguments(b);
+    return f;
+  }
 
 }
--- app/src/main/java/com/distantfuture/castvideos/app/settings/CastPreference.java
+++ app/src/main/java/com/distantfuture/castvideos/app/settings/CastPreference.java
@@ -16,10 +16,6 @@
 
 package com.distantfuture.castvideos.app.settings;
 
-import com.distantfuture.castcompanionlibrary.lib.cast.VideoCastManager;
-import com.distantfuture.castvideos.app.CastApplication;
-import com.distantfuture.castvideos.app.R;
-import com.distantfuture.castvideos.app.utils.Utils;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
@@ -29,99 +25,99 @@ import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
-public class CastPreference extends PreferenceActivity
-        implements OnSharedPreferenceChangeListener {
+import com.distantfuture.castcompanionlibrary.lib.cast.VideoCastManager;
+import com.distantfuture.castvideos.app.CastApplication;
+import com.distantfuture.castvideos.app.R;
+import com.distantfuture.castvideos.app.utils.Utils;
 
-    public static final String APP_DESTRUCTION_KEY = "application_destruction";
-    public static final String FTU_SHOWN_KEY = "ftu_shown";
-    public static final String VOLUME_SELCTION_KEY = "volume_target";
-    public static final String TERMINATION_POLICY_KEY = "termination_policy";
-    public static final String STOP_ON_DISCONNECT = "1";
-    public static final String CONTINUE_ON_DISCONNECT = "0";
-    private ListPreference mVolumeListPreference;
-    private SharedPreferences mPrefs;
-    private VideoCastManager mCastManager;
-    boolean mStopOnExit;
-    private ListPreference mTerminationListPreference;
+public class CastPreference extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 
-    @SuppressWarnings("deprecation")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.application_preference);
-        getPreferenceScreen().getSharedPreferences().
-                registerOnSharedPreferenceChangeListener(this);
-        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        mCastManager = CastApplication.getCastManager(this);
+  public static final String APP_DESTRUCTION_KEY = "application_destruction";
+  public static final String FTU_SHOWN_KEY = "ftu_shown";
+  public static final String VOLUME_SELCTION_KEY = "volume_target";
+  public static final String TERMINATION_POLICY_KEY = "termination_policy";
+  public static final String STOP_ON_DISCONNECT = "1";
+  public static final String CONTINUE_ON_DISCONNECT = "0";
+  private ListPreference mVolumeListPreference;
+  private SharedPreferences mPrefs;
+  private VideoCastManager mCastManager;
+  boolean mStopOnExit;
+  private ListPreference mTerminationListPreference;
 
-        // -- Termination Policy -------------------//
-        mTerminationListPreference = (ListPreference) getPreferenceScreen().findPreference(
-                TERMINATION_POLICY_KEY);
-        mTerminationListPreference.setSummary(getTerminationSummary(mPrefs));
-        mCastManager.setStopOnDisconnect(mStopOnExit);
+  @SuppressWarnings("deprecation")
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    addPreferencesFromResource(R.xml.application_preference);
+    getPreferenceScreen().getSharedPreferences().
+        registerOnSharedPreferenceChangeListener(this);
+    mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+    mCastManager = CastApplication.getCastManager(this);
 
-        // -- Volume settings ----------------------//
-        mVolumeListPreference = (ListPreference) getPreferenceScreen()
-                .findPreference(VOLUME_SELCTION_KEY);
-        String volValue = mPrefs.getString(
-                VOLUME_SELCTION_KEY, getString(R.string.prefs_volume_default));
-        String volSummary = getResources().getString(R.string.prefs_volume_title_summary, volValue);
-        mVolumeListPreference.setSummary(volSummary);
+    // -- Termination Policy -------------------//
+    mTerminationListPreference = (ListPreference) getPreferenceScreen().findPreference(TERMINATION_POLICY_KEY);
+    mTerminationListPreference.setSummary(getTerminationSummary(mPrefs));
+    mCastManager.setStopOnDisconnect(mStopOnExit);
 
-        EditTextPreference versionPref = (EditTextPreference) findPreference("app_version");
-        versionPref.setTitle(getString(R.string.version, Utils.getAppVersionName(this)));
+    // -- Volume settings ----------------------//
+    mVolumeListPreference = (ListPreference) getPreferenceScreen().findPreference(VOLUME_SELCTION_KEY);
+    String volValue = mPrefs.getString(VOLUME_SELCTION_KEY, getString(R.string.prefs_volume_default));
+    String volSummary = getResources().getString(R.string.prefs_volume_title_summary, volValue);
+    mVolumeListPreference.setSummary(volSummary);
+
+    EditTextPreference versionPref = (EditTextPreference) findPreference("app_version");
+    versionPref.setTitle(getString(R.string.version, Utils.getAppVersionName(this)));
+  }
+
+  public static boolean isDestroyAppOnDisconnect(Context ctx) {
+    SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ctx);
+    return sharedPref.getBoolean(APP_DESTRUCTION_KEY, false);
+  }
+
+  @Override
+  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+    if (VOLUME_SELCTION_KEY.equals(key)) {
+      String value = sharedPreferences.getString(VOLUME_SELCTION_KEY, "");
+      String summary = getResources().getString(R.string.prefs_volume_title_summary, value);
+      mVolumeListPreference.setSummary(summary);
+    } else if (TERMINATION_POLICY_KEY.equals(key)) {
+      mTerminationListPreference.setSummary(getTerminationSummary(sharedPreferences));
+      mCastManager.setStopOnDisconnect(mStopOnExit);
     }
+  }
 
-    public static boolean isDestroyAppOnDisconnect(Context ctx) {
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ctx);
-        return sharedPref.getBoolean(APP_DESTRUCTION_KEY, false);
-    }
+  private String getTerminationSummary(SharedPreferences sharedPreferences) {
+    String valueStr = sharedPreferences.getString(TERMINATION_POLICY_KEY, "0");
+    String[] labels = getResources().getStringArray(R.array.prefs_termination_policy_names);
+    int value = CONTINUE_ON_DISCONNECT.equals(valueStr) ? 0 : 1;
+    mStopOnExit = value == 0 ? false : true;
+    return labels[value];
+  }
 
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-            String key) {
-        if (VOLUME_SELCTION_KEY.equals(key)) {
-            String value = sharedPreferences.getString(VOLUME_SELCTION_KEY, "");
-            String summary = getResources().getString(R.string.prefs_volume_title_summary, value);
-            mVolumeListPreference.setSummary(summary);
-        } else if (TERMINATION_POLICY_KEY.equals(key)) {
-            mTerminationListPreference.setSummary(getTerminationSummary(sharedPreferences));
-            mCastManager.setStopOnDisconnect(mStopOnExit);
-        }
-    }
+  public static boolean isFtuShown(Context ctx) {
+    SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ctx);
+    return sharedPref.getBoolean(FTU_SHOWN_KEY, false);
+  }
 
-    private String getTerminationSummary(SharedPreferences sharedPreferences) {
-        String valueStr = sharedPreferences.getString(TERMINATION_POLICY_KEY, "0");
-        String[] labels = getResources().getStringArray(R.array.prefs_termination_policy_names);
-        int value = CONTINUE_ON_DISCONNECT.equals(valueStr) ? 0 : 1;
-        mStopOnExit = value == 0 ? false : true;
-        return labels[value];
-    }
+  public static void setFtuShown(Context ctx) {
+    SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ctx);
+    sharedPref.edit().putBoolean(FTU_SHOWN_KEY, true).commit();
+  }
 
-    public static boolean isFtuShown(Context ctx) {
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ctx);
-        return sharedPref.getBoolean(FTU_SHOWN_KEY, false);
+  @Override
+  protected void onResume() {
+    if (null != mCastManager) {
+      mCastManager.incrementUiCounter();
     }
+    super.onResume();
+  }
 
-    public static void setFtuShown(Context ctx) {
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ctx);
-        sharedPref.edit().putBoolean(FTU_SHOWN_KEY, true).commit();
+  @Override
+  protected void onPause() {
+    if (null != mCastManager) {
+      mCastManager.decrementUiCounter();
     }
-
-    @Override
-    protected void onResume() {
-        if (null != mCastManager) {
-            mCastManager.incrementUiCounter();
-        }
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        if (null != mCastManager) {
-            mCastManager.decrementUiCounter();
-        }
-        super.onPause();
-    }
+    super.onPause();
+  }
 
 }
